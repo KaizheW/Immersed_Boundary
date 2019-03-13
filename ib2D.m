f890260848f3d32ed487aebc1ba992d1e4323e31
Kaizhe <wkz9528@gmail.com>
2019-03-13 16:31:33 -0400

    Initial codes

---
+++ ib2D.m
@@ -0,0 +1,38 @@
+%% Immersed Boundary Method, 2D
+% This script is the main program.
+
+%%
+global L N K rho mu tmax dt;
+global a;
+global h ip im Nb dtheta kp km;
+L = 1.0; % size
+N = 64; % mesh
+K = 1.0; % force constant
+rho = 1.0; % density
+mu = 0.01; % viscosity
+tmax = 1; 
+dt = 0.01;
+initialize;
+a = zeros(N,N,2,2); % fluid solver
+init_a;
+
+%%
+for clock=1:clockmax
+  XX=X+(dt/2)*interp(u,X);
+  ff=spread(Force(XX),XX);
+  [u,uu]=fluid(u,ff);
+  X=X+dt*interp(uu,XX);
+  
+  %animation:
+  vorticity=(u(ip,:,2)-u(im,:,2)-u(:,ip,1)+u(:,im,1))/(2*h);
+  contour(xgrid,ygrid,vorticity,values)
+  hold on
+  plot(X(:,1),X(:,2),'ko')
+  axis([0,L,0,L])
+  caxis(valminmax)
+  axis equal
+  axis manual
+  drawnow
+  hold off
+end
+%%
