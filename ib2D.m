756ee0389c5616ec8a16fa64e94cb5ea77b7f5de
Kaizhe Wang <wkz9528@gmail.com>
2019-03-26 23:33:49 -0400

    Update ib2D.m

--- ib2D.m
+++ ib2D.m
@@ -1,7 +1,7 @@
 %% Immersed Boundary Method, 2D
 % This script is the main program.
 
-%%
+%% Initialize Parameter
 clear
 global L N K rho mu tmax dt;
 global a;
@@ -22,12 +22,71 @@ Nb = ceil(pi*N); % generate a circle.
 dtheta = 2*pi/Nb;
 kp = [(2:Nb),1];
 km = [Nb,(1:(Nb-1))];
-initialize;
+clockmax = ceil(tmax/dt);
 
+%% Initialize Boundary and Flow Field
+% generate a circle of ribbon
+X = zeros(Nb,2);
+X(:,1) = L/2 + L/4*cos((1:Nb)*dtheta);
+X(:,2) = L/2 + L/4*sin((1:Nb)*dtheta);
+
+% velocity of fluid flow
+u=zeros(N,N,2);
+[y,x] = meshgrid(0:h:L-h,0:h:L-h);
+u(:,:,2) = sin(2*pi*x/L);
+
+% vorticity: v_x - u_y; contour plot vorticity.
+vorticity=(u(ip,:,2)-u(im,:,2)-u(:,ip,1)+u(:,im,1))/(2*h);
+dvorticity=(max(max(vorticity))-min(min(vorticity)))/5;
+values= (-10*dvorticity):dvorticity:(10*dvorticity);
+valminmax=[min(values),max(values)];
+
+set(gcf,'double','on');
+contour(x,y,vorticity,values);
+hold on
+plot(X(:,1),X(:,2),'ko');
+axis([0,L,0,L]);
+caxis(valminmax);
+axis equal
+axis manual
+drawnow;
+hold off
+
+%% 4D matrix, fluid solver
 a = zeros(N,N,2,2); % fluid solver
-init_a;
+for m1=0:(N-1)
+  for m2=0:(N-1)
+    a(m1+1,m2+1,1,1)=1;
+    a(m1+1,m2+1,2,2)=1;
+  end
+end
 
-%%
+for m1=0:(N-1)
+  for m2=0:(N-1)
+    if~(((m1==0)||(m1==N/2))&&((m2==0)||(m2==N/2)))
+      t=(2*pi/N)*[m1;m2];
+      s=sin(t);
+      ss=(s*s')/(s'*s);
+%     a(m1+1,m2+1,:,:)=a(m1+1,m2+1,:,:)-(s*s')/(s'*s);
+      a(m1+1,m2+1,1,1)=a(m1+1,m2+1,1,1)-ss(1,1);
+      a(m1+1,m2+1,1,2)=a(m1+1,m2+1,1,2)-ss(1,2);
+      a(m1+1,m2+1,2,1)=a(m1+1,m2+1,2,1)-ss(2,1);
+      a(m1+1,m2+1,2,2)=a(m1+1,m2+1,2,2)-ss(2,2);
+    end
+  end
+end
+
+for m1=0:(N-1)
+  for m2=0:(N-1)
+    t=(pi/N)*[m1;m2];
+    s=sin(t);
+    a(m1+1,m2+1,:,:)=a(m1+1,m2+1,:,:)...
+                    /(1+(dt/2)*(mu/rho)*(4/(h*h))*(s'*s));
+  end
+end
+
+
+%% Calculation
 for clock=1:clockmax
   XX=X+(dt/2)*interp(u,X);
   ff=spread(Force(XX),XX);
