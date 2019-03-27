05fd470d0b6d4c585af8cdffee287966426e1b05
Kaizhe <wkz9528@gmail.com>
2019-03-26 22:38:05 -0400

    vector

--- initialize.m
+++ initialize.m
@@ -1,34 +1,25 @@
 global L N tmax dt;
 global h ip im Nb dtheta;
 
+% generate a circle of ribbon
 clockmax = ceil(tmax/dt);
 X = zeros(Nb,2);
+X(:,1) = L/2 + L/4*cos((1:Nb)*dtheta);
+X(:,2) = L/2 + L/4*sin((1:Nb)*dtheta);
 
-for k=0:(Nb-1)
-  theta=k*dtheta;
-  X(k+1,1)=(L/2)+(L/4)*cos(theta);
-  X(k+1,2)=(L/2)+(L/4)*sin(theta);
-end
-
+% velocity of fluid flow
 u=zeros(N,N,2);
-for j1=0:(N-1)
-  x=j1*h;
-  u(j1+1,:,2)=sin(2*pi*x/L);
-end
+[y,x] = meshgrid(0:h:L-h,0:h:L-h);
+u(:,:,2) = sin(2*pi*x/L);
 
+% vorticity: v_x - u_y;
 vorticity=(u(ip,:,2)-u(im,:,2)-u(:,ip,1)+u(:,im,1))/(2*h);
 dvorticity=(max(max(vorticity))-min(min(vorticity)))/5;
 values= (-10*dvorticity):dvorticity:(10*dvorticity);
 valminmax=[min(values),max(values)];
-xgrid=zeros(N,N);
-ygrid=zeros(N,N);
-for j=0:(N-1)
-  xgrid(j+1,:)=j*h;
-  ygrid(:,j+1)=j*h;
-end
 
 set(gcf,'double','on')
-contour(xgrid,ygrid,vorticity,values)
+contour(x,y,vorticity,values)
 hold on
 plot(X(:,1),X(:,2),'ko')
 axis([0,L,0,L])
